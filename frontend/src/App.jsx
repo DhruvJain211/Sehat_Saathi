16d3d754ace4c5f006205d39df72f20e057c1c8b
DhruvJain211 <dhruvyahoo112@gmail.com>
2024-08-01 23:01:38 +0530

    footer

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import Footer from "./Component/footer";
+const App = () => {
+  return (
+    <div>
+      <div className="h-screen"></div>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
